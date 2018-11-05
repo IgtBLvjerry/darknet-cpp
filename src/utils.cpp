5662ace390c824d6ef47149bd2573f9c41288347
吕泽宇 <lll12399@qq.com>
2018-11-05 18:50:11 +0800

    day3

--- src/utils.cpp
+++ src/utils.cpp
@@ -14,3 +14,38 @@
 #include <sys/time.h>
 
 #include "utils.h"
+
+double what_time_is_it_now() {
+    struct timeval time;
+    if (gettimeofday(&time, NULL)) {
+        return 0;
+    }
+    return (double) time.tv_sec + (double) time.tv_usec * .000001;
+}
+
+int *read_intlist(char *gpu_list, int *ngpus, int d) {
+    int *gpus = 0;
+    if (gpu_list) {
+        int len = strlen(gpu_list);
+        *ngpus = 1;
+        for (int i = 0; i < len; ++i) {
+            if (gpu_list[i] == ',') ++*ngpus;
+        }
+        gpus = (int *) calloc(*ngpus, sizeof(int));
+
+        for (int i = 0; i < *ngpus; ++i) {
+            gpus[i] = atoi(gpu_list);
+            gpu_list = strchr(gpu_list, ',') + 1;
+        }
+    } else {
+        gpus = (int *) calloc(1, sizeof(float));
+        *gpus = d;
+        *ngpus = 1;
+    }
+    return gpus;
+}
+
+
+
+
+
